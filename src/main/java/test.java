30241819e73899a4db67d0011eabdf064dbe64ca
zzg <18905222325@163.com>
2017-11-14 16:37:35 +0800

    测试完毕

--- src/main/java/test.java
+++
@@ -1,7 +0,0 @@
-public class test {
-    public static void main(String[] args) {
-        System.out.println("cbcb");
-        System.out.println("ceshi");
-        System.out.println("cc");
-    }
-}
