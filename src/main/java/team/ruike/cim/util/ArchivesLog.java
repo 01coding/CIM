d9770afd0dbcafbbb384312c3d7e242ce0ba4be8
zhangzhenguo <18905222325@163.com>
2017-12-12 16:25:55 +0800

    添加操作日志AOP

--- src/main/java/team/ruike/cim/util/ArchivesLog.java
+++ src/main/java/team/ruike/cim/util/ArchivesLog.java
@@ -10,11 +10,9 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 public @interface ArchivesLog {
-
     /** 要执行的操作类型比如：add操作 **/
-    public String operationType() default "";
-
+    String operationType() default "";
     /** 要执行的具体操作比如：添加用户 **/
-    public String operationName() default "";
+    String operationName() default "";
 
 }
